0d829b6ad2cb8fdb38bbae7b60763fd13962661b
benrobo <alumonabenaiah71@gmail.com>
2024-05-25 11:36:37 +0100

    feat: Update GoogleAuth callback to handle errors and provide error messages

--- packages/api/src/services/auth.service.ts
+++ packages/api/src/services/auth.service.ts
@@ -4,7 +4,7 @@ import prisma from "../config/prisma";
 import HttpException from "../lib/exception";
 import shortUUID from "short-uuid";
 import env from "../config/env";
-import GoogleAuth from "../lib/google.auth";
+import GoogleAuth, { googleClient } from "../lib/google.auth";
 
 export default class AuthService {
   constructor() {}
@@ -23,86 +23,93 @@ export default class AuthService {
   }
 
   public async googleAuthCallback(req: Request, res: Response) {
-    const code = req.query.code as string;
-    const { tokens } = await GoogleAuth.callBack({ code });
+    try {
+      const code = req.query.code as string;
+      const { tokens } = await GoogleAuth.callBack({ code });
 
-    if (!tokens) {
-      // redirect to client with error param
-      res.redirect(`${env.CLIENT_URL}/auth?error=google_auth_failed`);
-      throw new HttpException(RESPONSE_CODE.ERROR, "Google Auth failed", 401);
-    }
+      if (!tokens || !tokens.id_token) {
+        // redirect to client with error param
+        res.redirect(`${env.CLIENT_URL}/auth?error=google_auth_failed`);
+        console.log("Google Auth failed");
+        return;
+      }
 
-    const { id_token } = tokens;
-    const ticket = await GoogleAuth.verifyIdToken({ idToken: id_token! });
+      const { id_token, refresh_token } = tokens;
+      const ticket = await GoogleAuth.verifyIdToken({ idToken: id_token! });
 
-    const payload = ticket.getPayload();
-    let email = payload?.email;
+      const payload = ticket.getPayload();
+      let email = payload?.email;
 
-    if (!email) {
-      // redirect to client with error param
-      res.redirect(`${env.CLIENT_URL}/auth?error=email_not_found`);
-      throw new HttpException(
-        RESPONSE_CODE.ERROR,
-        "Email not found in google response",
-        401
-      );
-    }
+      if (!email) {
+        // redirect to client with error param
+        res.redirect(`${env.CLIENT_URL}/auth?error=email_not_found`);
+        console.log("Email not found in Google Auth response");
+        return;
+      }
 
-    const user = await prisma.users.findFirst({
-      where: {
-        email,
-      },
-    });
-
-    if (!user) {
-      // create one
-      const uId = shortUUID.generate();
-      const username = email.split("@")[0];
-      const fullname = payload?.name;
-      const avatar = payload?.picture;
-
-      await prisma.users.create({
-        data: {
+      const user = await prisma.users.findFirst({
+        where: {
           email,
-          uId,
-          username,
-          fullname,
-          avatar,
-          google_ref_token: tokens?.refresh_token!,
+        },
+      });
+
+      if (!user) {
+        // create one
+        const uId = shortUUID.generate();
+        const username = email.split("@")[0];
+        const fullname = payload?.name;
+        const avatar = payload?.picture;
+
+        await prisma.users.create({
+          data: {
+            email,
+            uId,
+            username,
+            fullname,
+            avatar,
+            google_ref_token: refresh_token,
+          },
+        });
+
+        this.setCookie("token", tokens.access_token!, res);
+        this.setCookie("_uId", uId, res);
+
+        console.log(`User created: ${email}`);
+
+        res.redirect(`${env.CLIENT_URL}/dashboard`);
+        return;
+      }
+
+      // update
+      await prisma.users.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          google_ref_token: refresh_token,
         },
       });
 
       this.setCookie("token", tokens.access_token!, res);
-      this.setCookie("_uId", uId, res);
+      this.setCookie("_uId", user.uId, res);
 
-      console.log(`User created: ${email}`);
+      console.log(`User logged in with email: ${email}`);
 
       res.redirect(`${env.CLIENT_URL}/dashboard`);
-      return;
+    } catch (e: any) {
+      console.error(e);
+      const msg = e.message || "Google Auth failed";
+      // redirect to client with error param
+      res.redirect(
+        `${env.CLIENT_URL}/auth?error=google_auth_failed&msg=${msg}`
+      );
     }
-
-    // update
-    await prisma.users.update({
-      where: {
-        id: user.id,
-      },
-      data: {
-        google_ref_token: tokens.refresh_token,
-      },
-    });
-
-    this.setCookie("token", tokens.access_token!, res);
-    this.setCookie("_uId", user.uId, res);
-
-    console.log(`User logged in with email: ${email}`);
-
-    res.redirect(`${env.CLIENT_URL}/dashboard`);
   }
 
   public logout(req: Request, res: Response) {
     res.clearCookie("token");
     res.clearCookie("_uId");
 
-    res.redirect(env.CLIENT_URL);
+    res.redirect(env.CLIENT_URL + "/auth");
   }
 }
