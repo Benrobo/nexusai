7dda85fa8fe4612fd0b210d2177958bb0ad2c2f1
benrobo <alumonabenaiah71@gmail.com>
2024-05-25 18:32:33 +0100

    refactor: Update phone number validation for agent creation
    This commit updates the phone number validation for agent creation in the createAgentSchema function of the schema_validation.ts file. The validation now ensures that the phone number provided follows the format of a valid US phone number starting with the country code +1. This change improves data integrity and prevents the creation of agents with invalid phone numbers.

---
+++ packages/api/src/prisma/migrations/20240525160245_second/migration.sql
@@ -0,0 +1,43 @@
+/*
+  Warnings:
+
+  - The values [CUSTOMER_SUPPORT] on the enum `AgentPurpose` will be removed. If these variants are still used in the database, this will fail.
+  - You are about to drop the `Workspace` table. If the table is not empty, all the data it contains will be lost.
+  - You are about to drop the `WorkspaceSettings` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "AgentPurpose_new" AS ENUM ('ANTI_SCAM', 'AUTOMATED_CUSTOMER_SUPPORT', 'CHATBOT');
+ALTER TABLE "Agents" ALTER COLUMN "purpose" TYPE "AgentPurpose_new" USING ("purpose"::text::"AgentPurpose_new");
+ALTER TYPE "AgentPurpose" RENAME TO "AgentPurpose_old";
+ALTER TYPE "AgentPurpose_new" RENAME TO "AgentPurpose";
+DROP TYPE "AgentPurpose_old";
+COMMIT;
+
+-- AlterTable
+ALTER TABLE "Agents" ADD COLUMN     "country" TEXT,
+ADD COLUMN     "country_code" TEXT;
+
+-- DropTable
+DROP TABLE "Workspace";
+
+-- DropTable
+DROP TABLE "WorkspaceSettings";
+
+-- CreateTable
+CREATE TABLE "VerifiedPhoneNumbers" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "isInUse" BOOLEAN NOT NULL DEFAULT false,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "VerifiedPhoneNumbers_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "VerifiedPhoneNumbers_phone_key" ON "VerifiedPhoneNumbers"("phone");
+
+-- CreateIndex
+CREATE INDEX "VerifiedPhoneNumbers_userId_idx" ON "VerifiedPhoneNumbers"("userId");
