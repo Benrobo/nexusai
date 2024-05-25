3aa17a0efbe0bf55f5351e96bb605a4c642d8f27
benrobo <alumonabenaiah71@gmail.com>
2024-05-25 20:59:24 +0100

    refactor: Update utils.ts to include logout function
    This commit adds a new logout function to the utils.ts file. The logout function removes the "token" and "user" items from the localStorage and redirects the user to the logout endpoint of the API. This change improves the security of the application by ensuring that user data is properly cleared when logging out.

--- packages/app/src/lib/utils.ts
+++ packages/app/src/lib/utils.ts
@@ -2,6 +2,7 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import env from "@/config/env";
 
 dayjs.extend(relativeTime);
 
@@ -13,3 +14,9 @@ export function cn(...inputs: ClassValue[]) {
 export function numberWithCommas(x: number) {
   return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
 }
+
+export const logout = () => {
+  localStorage.removeItem("token");
+  localStorage.removeItem("user");
+  window.location.href = `${env.API_URL}/auth/logout`;
+};
