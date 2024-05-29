709272e2e79743a6eda74d946222d6f126c07245
benrobo <alumonabenaiah71@gmail.com>
2024-05-29 10:49:30 +0100

    updated

--- packages/shared/config/supported-countries.ts
+++ packages/shared/config/supported-countries.ts
@@ -7,6 +7,13 @@ const SUPPORTED_COUNTRIES = [
     currency: "USD",
     currency_symbol: "$",
   },
+  {
+    name: "Canada",
+    code: "CD",
+    dial_code: "+1",
+    currency: "CAD",
+    currency_symbol: "$CAD",
+  },
 ];
 
 export default SUPPORTED_COUNTRIES;
