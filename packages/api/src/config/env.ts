b33ee9bbd6c848d053b84aeb1f8b1615effbeffb
benrobo <alumonabenaiah71@gmail.com>
2024-05-31 05:08:05 +0100

    updated

--- packages/api/src/config/env.ts
+++ packages/api/src/config/env.ts
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const env = {
   NODE_ENV: process.env.NODE_ENV || "development",
   CLIENT_URL: process.env.CLIENT_URL || "http://localhost:4000",
@@ -6,6 +9,12 @@ const env = {
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
   GOOGLE_REDIRECT_URL: process.env.GOOGLE_REDIRECT_URI!,
+  TWILIO: {
+    ACCT_SID: process.env.TWILIO_ACCOUNT_SID!,
+    AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN!,
+    PHONE_NUMBER: process.env.TWILIO_PHONE_NUMBER!,
+    TWILIO_AREA_CODE: process.env.TWILIO_AREA_CODE!,
+  },
 };
 
 export default env;
