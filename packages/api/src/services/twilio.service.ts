a66220d26230480605c50089934f9c3d74008853
benrobo <alumonabenaiah71@gmail.com>
2024-05-31 05:08:13 +0100

    updated

---
+++ packages/api/src/services/twilio.service.ts
@@ -0,0 +1,37 @@
+import env from "../config/env";
+import twClient from "../config/twillio/twilio_client";
+
+export class TwilioService {
+  constructor() {}
+
+  // send sms
+  async sendSMS(to: string, body: string) {
+    try {
+      const msg = await twClient.messages.create({
+        body,
+        to,
+        from: env.TWILIO.PHONE_NUMBER,
+      });
+
+      return msg;
+    } catch (error) {
+      console.log("error", error);
+      return null;
+    }
+  }
+
+  async getAvailableNumbersForPurchase(country?: string) {
+    try {
+      const numbers = await twClient
+        .availablePhoneNumbers(country ?? "US")
+        .local.list({
+          limit: 20,
+        });
+
+      return numbers;
+    } catch (e: any) {
+      console.log("error", e);
+      return null;
+    }
+  }
+}
