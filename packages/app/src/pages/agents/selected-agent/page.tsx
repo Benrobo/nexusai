bff6bbb305afd46ee20ad9ede1f48e92994eac4e
benrobo <alumonabenaiah71@gmail.com>
2024-06-04 00:28:19 +0100

    updated

--- packages/app/src/pages/agents/selected-agent/page.tsx
+++ packages/app/src/pages/agents/selected-agent/page.tsx
@@ -1,9 +1,14 @@
 import AgentSidebar from "@/components/agents/sidebar";
-import { FlexRowStart } from "@/components/Flex";
+import { FlexColCenter, FlexRowCenter, FlexRowStart } from "@/components/Flex";
 import type { AgentType } from "@nexusai/shared/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import GeneralPage from "./general";
-import type { AgentActiveTabs } from "@/types";
+import type { AgentActiveTabs, ResponseData } from "@/types";
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate, useParams } from "react-router-dom";
+import { getAgent } from "@/http/requests";
+import { Spinner } from "@/components/Spinner";
+import toast from "react-hot-toast";
 
 interface IAgentInfo {
   id?: string;
@@ -22,11 +27,53 @@ interface IAgentInfo {
 }
 
 export default function SelectedAgent() {
+  const navigate = useNavigate();
   const [activeTab, setActiveTab] = useState<AgentActiveTabs>("general");
-  const [agentInfo, setAgentInfo] = useState<IAgentInfo | null>({
-    type: "ANTI_THEFT",
-    name: "Baaymax",
+  const params = useParams();
+  const agentId = params.id;
+  const [error, setError] = useState<string | null>(null);
+  const [agentInfo, setAgentInfo] = useState<IAgentInfo | null>(null);
+  const [pageloading, setPageLoading] = useState(true);
+  const getAgentQuery = useMutation({
+    mutationFn: async (agentId: string) => await getAgent(agentId!),
+    onSuccess: (data) => {
+      const resp = data as ResponseData;
+      setAgentInfo(resp.data);
+      setPageLoading(false);
+    },
+    onError: (error) => {
+      const err = (error as any).response.data as ResponseData;
+      setError(err.message);
+      setPageLoading(false);
+      toast.error(err.message);
+    },
   });
+
+  useEffect(() => {
+    if (agentId) getAgentQuery.mutate(agentId!);
+  }, [agentId]);
+
+  if (getAgentQuery.isPending || pageloading) {
+    return (
+      <FlexRowCenter className="w-full h-full">
+        <Spinner color="#000" />
+      </FlexRowCenter>
+    );
+  }
+
+  if (!agentInfo || error) {
+    return (
+      <FlexColCenter className="w-full h-full">
+        <p className="text-dark-100 font-ppReg">No agent found</p>
+        <button
+          onClick={() => navigate(-1)}
+          className="w-auto px-5 py-2 scale-[.90] rounded-md bg-dark-100 text-white-100 font-ppReg text-xs"
+        >
+          Go back
+        </button>
+      </FlexColCenter>
+    );
+  }
 
   return (
     <FlexRowStart className="w-full bg-white-200/20">
