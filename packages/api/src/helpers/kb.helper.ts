abb6b9ed892e3b18585b7f1069bd85b92d306d0e
benrobo <alumonabenaiah71@gmail.com>
2024-06-11 10:51:44 +0100

    refactor: Add content field to knowledge base data insertion
    The `KbHelper` class has been updated to include a `content` field in the knowledge base data insertion query. This allows for storing the content of the knowledge base entry along with other metadata. The changes align with the recent updates to the knowledge base data structure and enhance the functionality and flexibility of the application.

--- packages/api/src/helpers/kb.helper.ts
+++ packages/api/src/helpers/kb.helper.ts
@@ -6,22 +6,32 @@ interface AddKbData {
   title: string;
   type: string;
   embedding: number[];
+  content: string;
   created_at: Date;
   updated_at: Date;
 }
 
 export default class KbHelper {
   public static async addKnowledgeBaseData(props: AddKbData) {
-    const { id, user_id, title, type, embedding, created_at, updated_at } =
-      props;
+    const {
+      id,
+      user_id,
+      title,
+      type,
+      embedding,
+      created_at,
+      updated_at,
+      content,
+    } = props;
     await prisma.$executeRaw`
-          INSERT INTO public."knowledge_base_data" (id,"user_id", "type", title, embedding, created_at, updated_at) 
+          INSERT INTO public."knowledge_base_data" (id,"user_id", "type", title, embedding,content, created_at, updated_at) 
           VALUES (
             ${id},
             ${user_id},
             ${type}::"KnowledgeBaseType",
             ${title},
             ${embedding}::numeric[],
+            ${content},
             ${created_at},
             ${updated_at}
           )
