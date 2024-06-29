8acf36556d1e23070d86f42cc473d7024616ee83
benrobo <alumonabenaiah71@gmail.com>
2024-06-29 23:45:22 +0100

    refactor: Update AGENT_NAME to defaultAgentName in agent/config.ts
    This commit updates the AGENT_NAME constant to defaultAgentName in the agent/config.ts file. The change ensures that the default agent name is used consistently throughout the codebase. This improves clarity and maintainability by using a more descriptive and accurate variable name.

--- packages/api/src/data/agent/config.ts
+++ packages/api/src/data/agent/config.ts
@@ -3,12 +3,23 @@
 import type { DefaultIntents } from "../../types/agent.types";
 
 export const AGENT_NAME = "Cassie";
+export const defaultAgentName = "Nexus";
 
 export const DEFAULT_CALL_INTENTS = [
   "EMERGENCY",
   "GREETINGS",
   "REQUEST",
   "GOODBYE",
+  "FURTHER_REQUEST",
+  "HANDOVER",
+  "CALL_ESCALATION",
+] satisfies DefaultIntents[];
+
+// SALES ASSISTANT
+export const DEFAULT_SA_CALL_INTENTS = [
+  "GREETINGS",
+  "ENQUIRY",
+  "APPOINTMENT",
   "FURTHER_REQUEST",
   "HANDOVER",
   "CALL_ESCALATION",
