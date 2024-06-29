c3069f11416dcd7bbd2f54942cd98f86c15c04d9
benrobo <alumonabenaiah71@gmail.com>
2024-06-29 23:46:17 +0100

    refactor: Update match_embeddings function to accept kb_ids parameter
    This commit updates the match_embeddings function in the function.sql file to accept a kb_ids parameter. The kb_ids parameter is an array of knowledge base IDs that the function will match against. This modification allows for more targeted similarity search by specifying the knowledge bases to search within. It improves the flexibility and functionality of the match_embeddings function.

--- packages/api/src/sql/function.sql
+++ packages/api/src/sql/function.sql
@@ -1,13 +1,12 @@
--- Create function responsible for similarity search on embeddings.
--- The function is based on the cosine similarity between two vectors.
-
 -- Drop function
 DROP FUNCTION IF EXISTS match_embeddings;
+
 -- Create function
 CREATE OR REPLACE FUNCTION match_embeddings (
   query_embedding text,
   match_threshold float,
-  match_count int
+  match_count int,
+  kb_ids text[]
 )
 RETURNS TABLE (
   result json
@@ -17,15 +16,13 @@ AS $$
   SELECT json_build_object(
     'id', public."knowledge_base_data".id,
     'similarity', 1 - (public."knowledge_base_data".embedding <=> query_embedding::vector),
-    'content', string_to_array(public."knowledge_base_data".content, '.')
-    -- 'metadata', public."DatasourceMetaData".metadata::json,
+    'content', string_to_array(public."knowledge_base_data".content, '.'),
+    'metadata', public."knowledge_base_data".title
   ) AS result
   FROM
     public."knowledge_base_data"
-    -- Metadata wasn't included in our schema, but would give this a try.
---   INNER JOIN 
---     public."DatasourceMetaData" ON public."Datasource".id = public."DatasourceMetaData".data_source_id
   WHERE
+    public."knowledge_base_data".kb_id = ANY(kb_ids) AND
     1 - (public."knowledge_base_data".embedding <=> query_embedding::vector) > match_threshold
   ORDER BY
     1 - (public."knowledge_base_data".embedding <=> query_embedding::vector) DESC
