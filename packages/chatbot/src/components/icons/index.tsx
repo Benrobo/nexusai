0434e2104bbb564e44d142229d8ccfce5d434eac
benrobo <alumonabenaiah71@gmail.com>
2024-07-19 12:29:48 +0100

    refactor: Add BellRing icon component to icons library

--- packages/chatbot/src/components/icons/index.tsx
+++ packages/chatbot/src/components/icons/index.tsx
@@ -550,3 +550,12 @@ export const RefreshCw = (props: SvgIconProps) => (
     <path d="M8 16H3v5" />
   </IconMarkup>
 );
+
+export const BellRing = (props: SvgIconProps) => (
+  <IconMarkup {...props}>
+    <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+    <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+    <path d="M4 2C2.8 3.7 2 5.7 2 8" />
+    <path d="M22 8c0-2.3-.8-4.3-2-6" />
+  </IconMarkup>
+);
