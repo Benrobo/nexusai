dc27293acd5525c60e890b234d7a3dace7a2b15d
benrobo <alumonabenaiah71@gmail.com>
2024-07-21 03:15:24 +0100

    feat: Add TelegramHelper class for handling Telegram group history and content

--- packages/api/src/helpers/telegram.helper.ts
+++ packages/api/src/helpers/telegram.helper.ts
@@ -1,1 +1,66 @@
 // telegram helper methods
+import shortUUID from "short-uuid";
+import prisma from "../prisma/prisma.js";
+
+export default class TelegramHelper {
+  public async getTelegramGroupHistory(groupId: string) {
+    const history = await prisma.telegramGroupHistory.findFirst({
+      where: { group_id: groupId },
+      include: { group_content: true },
+    });
+
+    return history;
+  }
+
+  public async getTelegramGroupHistoryText(groupId: string) {
+    const history = await this.getTelegramGroupHistory(groupId);
+    let historyTxt = "";
+
+    if (history) {
+      history.group_content.slice(-10).forEach((content) => {
+        historyTxt += `[${content.role}]: ${content.content}\n`;
+      });
+    }
+
+    return historyTxt;
+  }
+
+  public async storeTelegramGroupHistory(data: {
+    groupId: string;
+    content: string;
+    role: "user" | "bot";
+  }) {
+    const { groupId, content, role } = data;
+    const historyExists = await this.getTelegramGroupHistory(groupId);
+
+    if (historyExists) {
+      const historyContent = await prisma.telegramGroupHistoryContent.create({
+        data: {
+          id: shortUUID.generate(),
+          groupHistoryId: historyExists.id,
+          content,
+          role,
+        },
+      });
+
+      return historyContent;
+    } else {
+      const hId = shortUUID.generate();
+      const history = await prisma.telegramGroupHistory.create({
+        data: {
+          id: hId,
+          group_id: groupId,
+          group_content: {
+            create: {
+              id: shortUUID.generate(),
+              content,
+              role,
+            },
+          },
+        },
+      });
+
+      return history;
+    }
+  }
+}
