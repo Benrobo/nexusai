daf3093b1f7fdd9d81252eb2d5951b4c8bb0fad8
benrobo <alumonabenaiah71@gmail.com>
2024-07-27 20:09:53 +0100

    updated

--- packages/app/src/config/env.ts
+++ packages/app/src/config/env.ts
@@ -2,6 +2,8 @@ const inDev = import.meta.env.DEV;
 
 const env = {
   CLIENT_BASE_URL: inDev ? "http://localhost:4000" : "",
-  API_URL: inDev ? "http://localhost:4001/api" : "",
+  API_URL: inDev
+    ? "http://localhost:4001/api"
+    : "https://nexusai-ecow3.ondigitalocean.app/api",
 };
 export default env;
