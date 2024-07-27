4cddb7efb51f4cd4aea1004c2d5442b94ec6d57c
benrobo <alumonabenaiah71@gmail.com>
2024-07-27 22:15:33 +0100

    chore: Update CLIENT_BASE_URL in env.ts

--- packages/chatbot/src/config/env.ts
+++ packages/chatbot/src/config/env.ts
@@ -1,7 +1,9 @@
 const inDev = import.meta.env.DEV;
 
 const env = {
-  CLIENT_BASE_URL: inDev ? "http://localhost:3010" : "",
+  CLIENT_BASE_URL: inDev
+    ? "http://localhost:3010"
+    : "https://nexusai-chatbot.vercel.app/",
   API_URL: inDev
     ? "http://localhost:4001/api"
     : "https://nexusai-ecow3.ondigitalocean.app/api",
